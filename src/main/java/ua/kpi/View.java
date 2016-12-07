0a0be894fdfc6018fc76dad5f61e17cb7c3bf9c6
jekafortuna <evgeniy.fortuna@gmail.com>
2016-12-07 09:30:10 +0200

    New methods

--- src/main/java/ua/kpi/View.java
+++ src/main/java/ua/kpi/View.java
@@ -14,6 +14,24 @@ public class View {
     public static final String RESULT = "Result is: ";
     public static final String CHOOSE_OPERATION = "\nENTER NUMBER 1...24 to see the task and it solution. Or press 25 to quit!";
     public static final String BREAK = "BREAK!";
+    public static final String TASK1 = "Task1: Calculate the sum of all elements in array:";
+    public static final String TASK2 = "Task2: ";
+    public static final String TASK3 = "Task3: ";
+    public static final String TASK4 = "Task4: ";
+    public static final String TASK5 = "Task5: ";
+    public static final String TASK6 = "Task6: ";
+    public static final String TASK7 = "Task7: ";
+    public static final String TASK8 = "Task8: ";
+    public static final String TASK9 = "Task9: ";
+    public static final String TASK10 = "Task10: ";
+    public static final String TASK11 = "Task11: ";
+    public static final String TASK12 = "Task12: ";
+    public static final String TASK13 = "Task13: ";
+    public static final String TASK14 = "Task14: ";
+    public static final String TASK15 = "Task15: ";
+    public static final String TASK16 = "Task16: ";
+    public static final String TASK17 = "Task17: ";
+    public static final String TASK18 = "Task18: ";
 
     // Methods for printing the output data
     public void printMessage(Object message){
